e6c7df28339bdd1a8b4efa77486ef565dd056794
LeonLahi <111578448+LeonLahi@users.noreply.github.com>
2026-06-27 16:39:19 +0100

    infra: add swagger token authorization

--- iap.API/Program.cs
+++ iap.API/Program.cs
@@ -9,6 +9,10 @@ using iap.API.Filters;
 using iap.API.MIddleware;
 using Microsoft.AspNetCore.Identity;
 using iap.API.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,8 +23,71 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Add Identity services
+
+// //  Only needs to be added once 
+// builder.Services.AddIdentity<User, IdentityRole<int>>(options =>
+// {
+//     options.Password.RequireDigit = true;
+//     options.Password.RequiredLength = 8;
+//     options.Password.RequireUppercase = true;
+//     options.Password.RequireNonAlphanumeric = true;
+// })
+// .AddEntityFrameworkStores<IapDbContext>()
+// .AddDefaultTokenProviders();
+
 builder.Services.AddIdentityApiEndpoints<User>()
     .AddEntityFrameworkStores<IapDbContext>();
+
+
+// JWT Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+        ValidAudience = builder.Configuration["JwtSettings:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"]!))
+    };
+});
+
+// Add JWT to swagger 
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Enter JWT token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 // Add fluent validation
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
